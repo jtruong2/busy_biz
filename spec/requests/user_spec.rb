8245484e705c99954491cc5884fdab4e6c27376f
Jimmy <jtruong2@comcast.net>
2020-08-12 21:20:29 -0600

    test: adds sad path testing for Users Create

--- spec/requests/user_spec.rb
+++ spec/requests/user_spec.rb
@@ -13,5 +13,23 @@ RSpec.describe "User API" do
             expect(User.count).to eq(1)
             expect(output['username']).to eq(user.username)
         end
+
+        it "save fails without password" do
+            user = build(:user)
+            params = { username: user.username }
+      
+            post "/api/v1/users", params: params
+      
+            expect(User.count).to eq(0)
+          end
+      
+        it "save fails without username" do
+            user = build(:user)
+            params = { password: "password" }
+      
+            post "/api/v1/users", params: params
+      
+            expect(User.count).to eq(0)
+        end
     end
 end
